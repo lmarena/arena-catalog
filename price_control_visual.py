0caf28e31dd17190b85356714a782b11a898cfb1
Jackie Lian <86454986+Jackie-Lian@users.noreply.github.com>
2024-11-02 21:18:38 -0700

    Updated price_control_visualization function

--- price_control_visual.py
+++ price_control_visual.py
@@ -1,14 +1,15 @@
 import plotly.graph_objects as go
 
 
-def visualize_price_control(models, overall_scores, price_control_scores, lower_bound, upper_bound, reverse):
+def visualize_price_control(models, before, after, lower_bound, upper_bound, 
+                            reverse=False, criteria="Arena Score"):
     # Create traces for each model
     fig = go.Figure()
 
     for i, model in enumerate(models):
         fig.add_trace(go.Scatter(
             x=["Overall", "Overall + Price Control"],
-            y=[overall_scores[i], price_control_scores[i]],
+            y=[before[i], after[i]],
             mode='lines+markers',
             name=model,
             line=dict(width=2),
@@ -21,7 +22,7 @@ def visualize_price_control(models, overall_scores, price_control_scores, lower_
     fig.update_layout(
         title="Overall Ranking + Price Control",
         xaxis_title="Category",
-        yaxis_title="Performance (Arena Score)",
+        yaxis_title=f"Performance ({criteria})",
         xaxis=dict(tickmode='array', tickvals=["Overall", "Overall + Price Control"]),
         yaxis=dict(range=bounds),
         plot_bgcolor='black',
@@ -49,7 +50,7 @@ models = [
 overall_scores = [1299.05, 1286.49, 1274.06, 1270.62, 1270.09, 1266.46, 1262.54, 1259.56, 1257.46, 1251.57]
 price_control_scores = [1243.02, 1200, 1283.84, 1188.3, 1296.86, 1214.66, 1192.53, 1208.59, 1156.26, 1148.69]
 
-visualize_price_control(models, overall_scores, price_control_scores, 1140, 1310, False)
+visualize_price_control(models, overall_scores, price_control_scores, 1140, 1310)
 
 
 # +
@@ -61,7 +62,7 @@ top_overall_scores = [1270.09, 1274.06, 1227.15, 1220.17, 1299.05, 1248.64, 1248
 top_price_control_scores = [1296.857936, 1283.841603, 1261.443247, 1253.151069, 1243.015784, 1241.946454,
  1240.487616, 1239.54148, 1218.872415, 1216.253038]
 
-visualize_price_control(top_models, top_overall_scores, top_price_control_scores, 1190, 1310, False)
+visualize_price_control(top_models, top_overall_scores, top_price_control_scores, 1190, 1310)
 # -
 # Ranking change with ranking on the y-axis rather than scores
 
@@ -72,7 +73,7 @@ models = ['gemini-1.5-flash-exp-0827', 'gpt-4o-mini-2024-07-18', 'gemini-1.5-fla
 ranks_before = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 ranks_after = [5, 3, 18, 19, 1, 12, 13, 21, 30, 20]
 
-visualize_price_control(models, ranks_before, ranks_after, 0, 35, True)
+visualize_price_control(models, ranks_before, ranks_after, 0, 35, True, "Rank")
 
 models = ['gemini-1.5-flash-exp-0827', 'gpt-4o-mini-2024-07-18', 'gemini-1.5-flash-api-0514',
  'deepseek-v2-api-0628', 'gemini-1.5-pro-exp-0827', 'athene-70b-0725', 
@@ -80,6 +81,6 @@ models = ['gemini-1.5-flash-exp-0827', 'gpt-4o-mini-2024-07-18', 'gemini-1.5-fla
  'gemma-2-27b-it']
 ranks_before = [5, 3, 18, 19, 1, 12, 13, 21, 30, 20]
 ranks_after = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-visualize_price_control(models, ranks_before, ranks_after, 0, 35, True)
+visualize_price_control(models, ranks_before, ranks_after, 0, 35, True, "Rank")
 
 
